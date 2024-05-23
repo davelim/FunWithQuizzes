d484b09879e4f1286c74fee8fea49b8afdaab747
Dave Lim <davelim77@gmail.com>
2024-05-22 22:49:19 -0500

    Add support for CheckBox questions

--- Impl/Program.cs
+++ Impl/Program.cs
@@ -9,11 +9,11 @@ Question q1 = new MultipleChoice(
     ["class", "static", "type", "delegate", "this"],
     "static");
 quiz.AddQuestion(q1);
-// Question q2 = new CheckBox(
-//     "What are some access modifiers?",
-//     ["public", "protected internal", "protected", "internal", "private protected", "private", "file", "assembly"],
-//     ["public", "protected internal", "protected", "internal", "private protected", "private", "file"]);
-// quiz.AddQuestion(q2);
+Question q2 = new CheckBox(
+    "What are C# access modifiers?",
+    ["public", "protected internal", "protected", "internal", "private protected", "private", "file", "assembly"],
+    ["public", "protected internal", "protected", "internal", "private protected", "private", "file"]);
+quiz.AddQuestion(q2);
 // Question q3 = new TrueFalse(
 //     "We have to declare a private field when we declare a public property.",
 //     false
---
+++ Impl/Questions/CheckBox.cs
@@ -0,0 +1,85 @@
+using System.Text;
+namespace FunWithQuizzes;
+
+public class CheckBox : Question
+{
+    // field(s)/prop(s)
+    public readonly List<string> _choices;
+    private readonly string _choicesIndices;
+    private readonly List<string> _correctAnswers;
+    // constructor(s)
+    public CheckBox(string questionStr, List<string> choices, List<string> correctAnswers)
+    : base(questionStr)
+    {
+        _choices = choices;
+        _choicesIndices = String.Join(", ", Enumerable.Range(0, _choices.Count));
+        _correctAnswers = correctAnswers;
+    }
+    // override(s)
+    // TODO: AskNGetAnswer() and Score(string) are abstract methods that
+    //   return a string and accept a string as a parameter. This is
+    //  inefficient for CheckBox question. Fix.
+    public override string AskNGetAnswer()
+    {
+        List<int> userChoices;
+        Console.WriteLine(BuildQuestion());
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            userChoices = ParseInts(input);
+            Console.WriteLine($"Please confirm you chose: {String.Join(", ", userChoices)} [y]");
+            input = Console.ReadLine();
+            if (input == null || input.Equals("")
+                || input.Contains("y") || input.Contains("Y"))
+            {
+                break;
+            }
+        }
+        return String.Join(" ", userChoices);
+    }
+    public override int Score(string answer)
+    {
+        int score = 0;
+        foreach (string s in answer.Split(" "))
+        {
+            if (_correctAnswers.Contains(_choices[int.Parse(s)]))
+            {
+                score += 1;
+            }
+        }
+        return score;
+    }
+    // methods(s)
+    private List<int> ParseInts(string? str)
+    {
+        List<int> ints = new();
+        if (str == null || str.Equals(""))
+        {
+            return ints;
+        }
+        for (int i=0; i<str.Length; i++)
+        {
+            int parsedInt;
+            if (int.TryParse(str[i].ToString(), out parsedInt))
+            {
+                if (!ints.Contains(parsedInt))
+                {
+                    ints.Add(parsedInt);
+                }
+            }
+        }
+        return ints;
+    }
+    private StringBuilder BuildQuestion()
+    {
+        StringBuilder sb = new();
+        sb.AppendLine(Constants.DASHED_LINE);
+        sb.AppendLine(_questionStr);
+        sb.AppendLine($"Please choose all that apply [{_choicesIndices}]");
+        for (int i = 0; i < _choices.Count; i++)
+        {
+            sb.AppendLine($"> {i} - {_choices[i]}");
+        }
+        return sb;
+    }
+}
